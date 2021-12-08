79c24a52d40296b9ef5f1401506a151acfef65c8
PujaPatel <ppate343@uwo.ca>
2021-12-07 21:33:03 -0500

    added views

--- Views/MainPageView.java
+++ Views/MainPageView.java
@@ -1,1 +1,56 @@
 package Views;
+
+
+public class MainPageView {
+
+    private models.CommentModel model;
+
+    controllers.GradeController gradeController; 
+
+    public MainPageView(models.CommentModel model,  controllers.GradeController gradeController) {
+
+        this.setModel(model);
+        this.setController(gradeController); 
+    }
+
+
+    public void addComment() {
+
+        //collects comment data from the user 
+
+            String comments = "3352 data comment"; 
+
+    }
+
+    public MainPageView() {
+
+    }
+
+    public void show() {
+        // display or refresh page 
+
+    }
+
+    //getModel method returns the value of comment model
+    public models.CommentModel getModel(){
+        return model; 
+    }
+
+    //setModel method sets the value of comment Model to model
+    public void setModel(models.CommentModel model) {
+        this.model = model; 
+    }
+    //getController method returns the value of grade controller 
+    public controllers.GradeController getController() {
+        return gradeController; 
+    }
+
+    //setController method sets the value of grade controller to gradeController
+     public void setController(controllers.GradeController gradeController) {
+        this.gradeController = gradeController; 
+
+    }
+
+
+
+}
--- Views/SubpageView.java
+++ Views/SubpageView.java
@@ -1,5 +1,77 @@
 package Views;
+import javax.swing.*;
 
 public class SubpageView {
     
+    private models.ContentLinkModel model; 
+    private models.EmbeddedContent eModel; 
+    private controllers.addContentController controller; 
+
+    public SubpageView (models.ContentLinkModel model,models.EmbeddedContent eModel, controllers.addContentController controller ) {
+        
+        this.setModel(model);   
+
+        this.seteModel(eModel); 
+         
+        this.setController(controller); 
+
+    }
+
+    public void addContentButton() {
+
+        //allows user to click button to upload content 
+            JButton content = new JButton("addContent"); 
+
+
+    }
+
+    public void addEmbeddedContentButton() {
+
+        //allows user to click button to upload embeddedContent 
+        JButton embeddedContent = new JButton("addEmbeddedContent"); 
+    }
+
+    public SubpageView() {
+
+    }
+
+    public void show() {
+        // display or refresh page 
+
+    }
+
+    //geteModel method returns the value of embedded content model 
+    public models.EmbeddedContent geteModel(){
+        return eModel; 
+    }
+
+    //seteModel method sets the value of embedded content model to model 
+    public void seteModel(models.EmbeddedContent eModel) {
+        this.eModel = eModel; 
+    }
+
+    //getModel method returns the value of content link model 
+    public models.ContentLinkModel getModel() {
+        return model; 
+    }
+
+    //setModel method sets the value of content link model to model 
+    public void setModel(models.ContentLinkModel model) {
+        this.model = model; 
+
+    }
+
+    //getController method returns the value of add content controller to controller
+    public controllers.addContentController getController() {
+        return controller; 
+
+    }
+
+    //setcontroller method sets the value of add content controller to controller
+    public void setController (controllers.addContentController controller) {
+        this.controller = controller; 
+    }
+
+
+
 }
