3148b34d8065afc8276a7891e0b3a9a224f57b63
Robert <49911127+rfreeman203@users.noreply.github.com>
2021-12-07 21:02:28 -0500

    started addContentController

--- controllers/addContentController.java
+++ controllers/addContentController.java
@@ -1,5 +1,53 @@
 package controllers;
 
+
 public class addContentController {
+
+    models.TextModel textModel;
+    models.ContentLinkModel contentLinkModel;
+    models.EmbeddedContentModel embeddedContentModel;
+
+    Views.LessonsView lessonsView;
+    Views.MainPageView mainPageView;
+    Views.SubpageView subpageView;
+
     
+    public addContentController(Views.LessonsView lessonsView, models.TextModel textModel, models.ContentLinkModel contentLinkModel, models.EmbeddedContentModel embeddedContentModel){
+        this.lessonsView = lessonsView;
+        this.textModel = textModel;
+        this.contentLinkModel = contentLinkModel;
+        this.embeddedContentModel = embeddedContentModel;
+
+        
+    }
+
+    public addContentController(Views.MainPageView mainPageView, models.TextModel textModel, models.ContentLinkModel contentLinkModel, models.EmbeddedContentModel embeddedContentModel){
+        this.mainPageView = mainPageView;
+        this.textModel = textModel;
+        this.contentLinkModel = contentLinkModel;
+        this.embeddedContentModel = embeddedContentModel;
+
+        
+    }
+
+    public addContentController(Views.SubpageView subpageView, models.TextModel textModel, models.ContentLinkModel contentLinkModel, models.EmbeddedContentModel embeddedContentModel){
+        this.subpageView = subpageView;
+        this.textModel = textModel;
+        this.contentLinkModel = contentLinkModel;
+        this.embeddedContentModel = embeddedContentModel;
+
+        
+    }
+
+    public void updateLessons(){
+
+    }   
+
+    public void updateMainPage(){
+
+    }  
+
+    public void updateSubPage(){
+
+    }  
 }
